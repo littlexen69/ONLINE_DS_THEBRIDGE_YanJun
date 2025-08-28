700af02bc7935397c49e9fc0ffb1bdbeadc7bf1c
littlexen69 <yanjunchen69@gmail.com>
2025-08-28 20:47:42 +0200

    h

---
+++ Machine Learning/Stock_Prediction_ML/src/evaluation.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.preprocessing import MinMaxScaler
+
+from training import train_test_split, xgb_prediction
+
+df = pd.read_csv('../data/processed/AAPL.US.csv')
+df.index = df["date"]
+df.drop("date", axis=1, inplace=True)
+
+last_row = df.tail(1)
+df.drop(df.tail(1).index, inplace=True)
+df.dropna(inplace=True)
+
+feature_scaler = MinMaxScaler()
+target_scaler = MinMaxScaler()
+
+def plot_values(df, percentage=0.2):
+
+    train, test = train_test_split(df, percentage)
+    X, Y = train[:, :-1], train[:, -1]
+    train_predictions = model.predict(X)
+    
+    # desescalar
+    train_predictions = target_scaler.inverse_transform(train_predictions.reshape(-1, 1))
+    Y = target_scaler.inverse_transform(Y.reshape(-1, 1))
+    train_rmse = metrics.root_mean_squared_error(Y, train_predictions)
+    
+    print(f"Training RMSE: {train_rmse}")
+    print(f"Testing RMSE: {test_rmse}")
+
+    # plot actual vs. predicted
+    # concatenar Y e Y_test para plotearlo junto
+    Y = Y.reshape(-1)
+    train_predictions = train_predictions.reshape(-1)
+    Y = np.concatenate((Y, Y_test.reshape(-1)), axis=0)
+    new_predictions = np.concatenate((train_predictions, predictions.reshape(-1)), axis=0)
+    plt.plot(new_predictions, label='Predicted')
+    plt.plot(Y, label='Actual')
+    plt.legend()
+    plt.show()
+
+prediction = xgb_prediction(df.values, last_row.values[0][:-1])
+print(prediction)
