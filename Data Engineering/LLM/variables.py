60b5ac9a06b984178a5dcf166041a312acad85a5
Yan Jun Chen Ye <yanjunchen69@gmail.com>
2025-09-16 09:23:38 +0200

    Actualizar variables.py

--- Data Engineering/LLM/variables.py
+++ Data Engineering/LLM/variables.py
@@ -3,9 +3,9 @@ import os
 os.environ['KEY_GROQ'] = ""
 
 config = {
-    "host":"prueba.cf2yw4804ah0.eu-west-3.rds.amazonaws.com",
-    "user":"postgres",
-    "password":"Hola1234",
-    "port":5432,
-    "dbname": "postgres"
+    "host":"",
+    "user":"",
+    "password":"",
+    "port":,
+    "dbname": ""
 }
