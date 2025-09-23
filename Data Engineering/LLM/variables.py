554146a057d3a6c443e1156d68085172266bb1f8
littlexen69 <yanjunchen69@gmail.com>
2025-09-23 11:51:24 +0200

    a

--- Data Engineering/LLM/variables.py
+++ Data Engineering/LLM/variables.py
@@ -1,11 +1,14 @@
 import os
+from dotenv import load_dotenv
 
-os.environ['KEY_GROQ'] = ""
+load_dotenv()
+
+os.getenv("KEY_GROQ")
 
 config = {
-    "host":"",
-    "user":"",
-    "password":"",
-    "port":,
-    "dbname": ""
+    "host":os.getenv("DB_HOST"),
+    "user":os.getenv("DB_USER"),
+    "password":os.getenv("DB_PASSWORD"),
+    "port":int(os.getenv("DB_PORT")),
+    "dbname": os.getenv("DB_NAME")
 }
